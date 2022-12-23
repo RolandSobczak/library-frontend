003c4309121372aca2cca772331b0777b5fb3e0c
Roland Sobczak <rolandsobczak@gmail.com>
2022-12-23 18:37:13 +0100

    New Dashboard page

---
+++ src/components/Main/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Library project</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
